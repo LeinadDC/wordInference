2c78b5f58a4a66756f36c7bf0976c1b69465a83c
Daniel Castellano <daniel.castellanom@gmail.com>
2017-10-23 19:56:24 -0600

    Agregada informacion para busqueda

--- wordInference.py
+++ wordInference.py
@@ -5,7 +5,7 @@ ama que pasaba de los cuarenta, y una sobrina que no llegaba a los veinte,
 y un mozo de campo y plaza, que así ensillaba el rocín como tomaba la
 podadera. Frisaba la edad de nuestro hidalgo con los cincuenta años; era de
 complexión recia, seco de carnes, enjuto de rostro, gran madrugador y amigo
-de la caza."""
+de la caza. Cazar estaba ido en el mar, cuando de pronto me encontré nadando"""
 
 
 test = oracion.split(" ")
@@ -14,6 +14,7 @@ palabrasNoAba = []
 pronombresList = []
 
 ### Formas No Personales
+formasNoPersonales = ['ar','er','ir','or','ur','ndo','ado','ido','to','so','cho','aba']
 infinitivos = ['ar','er','ir','or','ur']
 gerundios = ['ndo']
 participio = ['ado','ido','to','so','cho']
@@ -25,7 +26,7 @@ pronombres = ['yo','tu','vos','usted','el','ella','ello','nosotras','nosotros','
 
 
 for word in test:
-    if word.endswith('aba'):
+    if word.endswith(tuple(formasNoPersonales)):
         palabrasAba.append(word)
     elif word.lower() in pronombres:
         pronombresList.append(word)
@@ -33,7 +34,7 @@ for word in test:
         palabrasNoAba.append(word)
 
 
-print("Verbos en Pasado")
+print("Verbos")
 print(palabrasAba)
 print("Pronombres")
 print(pronombresList)
